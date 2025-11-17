abb4279671243e20be85ffa71ae02ed7fc1fa1a1
Saikrish <vijayasatyad@gmail.com>
2025-11-17 21:46:07 +0530

    new feature

--- src/langgraphagenticai/Ui/streamlitui/loadui.py
+++ src/langgraphagenticai/Ui/streamlitui/loadui.py
@@ -61,12 +61,19 @@ class LoadStreamlitUI:
                 #Validate API Key
                 if not self.user_controls["GROQ_API_KEY"]:
                     st.warning("Please Enter your GROQ API Key to Preoceed. Don't have? refer : https://console.groq.com/keys")
-
-                # Use case Selection
-                self.user_controls["selected_usecase"] = st.selectbox("Select Usecases", usecase_options)
-
-                if "state" not in st.session_state:
-                    st.session_state.state = self.initialize_session()
+                    
+            # Use case selection
+            self.user_controls["selected_usecase"] = st.selectbox("Select Usecases", usecase_options)
+            # Use case Selection
+            if self.user_controls["selected_usecase"] =="Chatbot with Tool" or self.user_controls["selected_usecase"] =="AI News" :
+                # API key input
+                os.environ["TAVILY_API_KEY"] = self.user_controls["TAVILY_API_KEY"] = st.session_state["TAVILY_API_KEY"] = st.text_input("TAVILY API KEY",
+                                                                                                      type="password")
+                # Validate API key
+                if not self.user_controls["TAVILY_API_KEY"]:
+                    st.warning("⚠️ Please enter your TAVILY_API_KEY key to proceed. Don't have? refer : https://app.tavily.com/home")
+            if "state" not in st.session_state:
+                st.session_state.state = self.initialize_session()
 
         #self.render_requirements()
         return self.user_controls
